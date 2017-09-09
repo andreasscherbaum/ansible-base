31ed2859e254a9c1f55a72f758c08b488375d2a7
Andreas Scherbaum <andreas@scherbaum.biz>
2017-09-10 00:20:21 +0200

    Move everything into template files Add way to include more repositories

--- README.md
+++ README.md
@@ -5,3 +5,11 @@ Base directory for Ansible playbooks
 ## Main usage
 
 That is primarily my own start for a new Ansible Playbook. But feel free to look around ...
+
+Start by copying the _Makefile.template_ to _Makefile_. This avoids conflicts with updates in the repository.
+
+```
+cp -a Makefile.template Makefile
+make all
+```
+
---
+++ available-roles.mk
@@ -0,0 +1,30 @@
+# this Makefile holds information about known Ansible roles
+# it can be updated from the repository, while the main "Makefile" can be changed
+
+available-roles:
+	@echo ""
+	@echo "Available roles:"
+	@echo ""
+	@echo "common:		a common Ansible role to install a host"
+	@echo "		https://github.com/andreasscherbaum/ansible-common"
+	@echo "gpdb-ansible:	an Ansible role to install Greenplum Database"
+	@echo "		https://github.com/andreasscherbaum/gpdb-ansible"
+
+
+common:
+ifneq  (,$(wildcard roles/common))
+	cd roles/common && git pull
+	ln -s roles/common/ansible.mk ansible-common.mk
+else
+	git clone https://github.com/andreasscherbaum/ansible-common.git roles/common
+endif
+
+
+gpdb:
+ifneq  (,$(wildcard roles/gpdb-ansible))
+	cd roles/gpdb-ansible && git pull
+	ln -s roles/gpdb-ansible/ansible.mk ansible-gpdb-ansible.mk
+else
+	git clone https://github.com/andreasscherbaum/gpdb-ansible.git roles/gpdb-ansible
+endif
+
